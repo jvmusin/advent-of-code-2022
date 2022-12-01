4fd455473f37969e1f10900d4e60a2564818e837
Rustam Musin <rustam.musin@jetbrains.com>
2022-12-01 09:50:04 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun readCalories(input: List<String>) = sequence {
+        var start = 0
+        while (start < input.size) {
+            var sum = 0
+            while (start < input.size) {
+                val line = input[start++]
+                if (line.isEmpty()) break
+                sum += line.toInt()
+            }
+            yield(sum)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = readCalories(input).max()
+
+    fun part2(input: List<String>) = readCalories(input).sortedDescending().take(3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
